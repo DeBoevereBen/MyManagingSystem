e3552a40aff8095917740d00620c82b3ea96d487
Ben De Boevere <ben@bendb.be>
2019-11-05 11:39:06 +0100

    init

---
+++ MyManagingSystem.Domain/Models/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyManagingSystem.Domain.Models
+{
+    public class Inventory
+    {
+        
+    }
+}
